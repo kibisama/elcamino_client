e033f73a2a8a1d173eb69fe7c35019f34cc90604
이현구 <kibisama9@gamil.com>
2025-12-09 12:13:47 -0800

    0.0.1

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create();
-client.defaults.baseURL = process.env.REACT_APP_CLIENT_API_ADDRESS;
+client.defaults.baseURL = "api";
 client.interceptors.request.use(
   (req) => {
     const access_token = localStorage.getItem("elcamino_client_access_token");
